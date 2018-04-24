6cd7bcf33b48b21b3ddafa22e5bac314bddf7253
Michael Angerman <stormasm@gmail.com>
2018-04-23 17:20:05 -0700

    demo-github up and running

--- table/src/index.js
+++ table/src/index.js
@@ -1,10 +1,9 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import Demo from './demo';
+import Demo from './demo-github';
 
 const rootElement = document.querySelector('#root');
 if (rootElement) {
   render(<Demo />, rootElement);
 }
-      

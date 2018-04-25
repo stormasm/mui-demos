29e0712b08b5e1e2d016da20311c733f802fcc9a
Michael Angerman <stormasm@gmail.com>
2018-04-25 12:13:58 -0700

    i think it is working

--- table/src/demo-github-json.js
+++ table/src/demo-github-json.js
@@ -20,6 +20,17 @@ const styles = theme => ({
   }
 });
 
+const template =
+  "https://raw.githubusercontent.com/stormasm/mui-demos/master/table/src/";
+
+const repoMap = {
+  repo1: "html5-node-diagram.json",
+  repo2: "ivy.json",
+  repo3: "nodejs-sandboxed-fs.json",
+  repo4: "ghme.json"
+};
+
+/*
 function createData(login, name, location) {
   return { login, name, location };
 }
@@ -29,36 +40,115 @@ const data = [
   createData("stormdock", "Storm Dock", "Santa Fe, NM"),
   createData("stormreact", "Storm React", "Buenos Aires")
 ];
+*/
+class SimpleTable extends React.Component {
+  constructor(props) {
+    super(props);
 
-function SimpleTable(props) {
-  const { classes } = props;
+    // const { classes } = props;
 
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>login</TableCell>
-            <TableCell>name</TableCell>
-            <TableCell>location</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((key, index) => {
-            return (
-              <TableRow key={index}>
-                <TableCell>
-                  <a href={"https://github.com/" + key.login}>{key.login}</a>
-                </TableCell>
-                <TableCell>{key.name}</TableCell>
-                <TableCell>{key.location}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+    this.state = {
+      data: {},
+      isLoading: false,
+      error: null,
+      repoName: repoMap['repo4']
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    /*
+    Do NOT Delete for awhile --- helpful for debugging...
+
+    const locationChanged = nextProps.location !== this.props.location;
+    console.log("componentWillUpdate");
+    console.log("locationChanged: ", locationChanged);
+    console.log(nextProps.location);
+    console.log(nextProps.match.params.repo);
+    console.log(nextProps.match.params.view);
+    console.log(repoMap[nextProps.match.params.repo]);
+*/
+    const url = template + repoMap[nextProps.match.params.repo];
+
+    this.setState({ isLoading: true });
+    this.setState({ repoName: repoMap['repo4'] });
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(
+            "Sorry, but something went wrong in the CardWrapper..."
+          );
+        }
+      })
+      .then(data => this.setState({ data, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    this.setState({ repoName: repoMap['repo4'] });
+
+    const url = template + this.state.repoName;
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(
+            "Sorry, but something went wrong in the CardWrapper..."
+          );
+        }
+      })
+      .then(data => this.setState({ data, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  render() {
+
+    const {
+      classes
+    } = this.props;
+
+    const hits = this.state.data.hits || [];
+
+    if (this.state.error) {
+      return <p>{this.state.error.message}</p>;
+    }
+
+    if (this.state.isLoading) {
+      return <p>Loading ...</p>;
+    }
+
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>login</TableCell>
+              <TableCell>name</TableCell>
+              <TableCell>location</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {hits.map((key, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>
+                    <a href={"https://github.com/" + key.login}>{key.login}</a>
+                  </TableCell>
+                  <TableCell>{key.name}</TableCell>
+                  <TableCell>{key.location}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 SimpleTable.propTypes = {

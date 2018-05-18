7c885af2bca3f388987d193bcea4aa4a63e32174
Michael Angerman <stormasm@gmail.com>
2018-05-18 11:07:02 -0700

    add checkboxlist directory

--- checkboxlist/src/index.js
+++ checkboxlist/src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import Demo from './EnhancedTable';
+import Demo from './CheckboxList';
 
 const rootElement = document.querySelector('#root');
 if (rootElement) {

593ebb782c94b1368102568baad803ea7928a3bd
Michael Angerman <stormasm@gmail.com>
2018-05-18 17:10:50 -0700

    pulling data from gh

--- checkboxlist/src/index.js
+++ checkboxlist/src/index.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import Demo from './CheckboxList';
+//import Demo from './CheckboxList';
+import Demo from './CheckboxList-gh';
 
 const rootElement = document.querySelector('#root');
 if (rootElement) {
